63fe0a8de81af295e39b0982d9128173c79434c4
Pogboigit <liammiddleton01@outlook.com>
2022-11-22 17:17:31 +0200

    Updating new github account

---
+++ Fibonacci+Factorial/Fibonacci/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //Prints the fibonacci sequence up to the tenth number
+            int first = 0;
+            int second = 1; 
+            int third = 0;
+            Console.Write("{0} {1}", first, second);
+            for (int i = 2; i < 10; i++)
+            {
+                third = first + second;
+                Console.Write(" {0}", third);
+                first = second;
+                second = third;
+            }
+
+            Console.WriteLine("\n " + Factorial(6)); 
+        }
+
+        static int Factorial(int n)
+        {
+            //Gets the factorial of a given number
+            int current = 1, i;
+
+            for (i = 2; i < n; i++)
+            {
+                current *= i;
+            }
+
+            return current;
+        }
+        
+    }
+}
